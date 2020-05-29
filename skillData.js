483cd3c76984ed6f8a9e4d46a7cdbb8364a04ee1
Raja Vikrant Sharma <shubham875@gmail.com>
2020-05-29 21:18:25 +0530

    Create skillData.js

---
+++ skillData.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "en-IN": {
+        "WELCOME_MESSAGE":"Welcome to Riddle Me Today. I am going to ask you a riddle every day, and 3 chances to answer it correctly. The Less Chances you Take, the more points you win. All your points count towards our weekly leaderboard.",
+        "QUESTION": "Are you ready for today's riddle?",
+        "RETURNING_USERS_WELCOME": "Welcome back to Riddle Me Today. ",
+        "QUESTIONS": [
+        "What has many keys but can't open a single lock?",
+        "What can travel around the world while staying in a corner?",
+        "The eight of us go forth not back",
+        "What can you catch but never throw?"
+        ],
+        "ANSWERS": [
+            "Piano",
+            "Stamp",
+            "Pawns",
+            "Cold"
+        ]
+    },
+    "hi-IN":{
+        "WELCOME_MESSAGE":"पहेली का खेल मैं आपका स्वागत हैं, और धन्यवाद",
+        "QUESTION": "क्या आप खेलने के लिए तैय्यार हैं?",   
+    }
+}
